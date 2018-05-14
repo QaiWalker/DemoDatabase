49b5d2648040d71d0f2da59d82ddf977347558eb
16014753 <16014753@myrp.edu.sg>
2018-05-14 15:05:39 +0800

    Final push

--- app/src/main/java/demodatabase/android/myapplicationdev/com/demodatabase/DBHelper.java
+++ app/src/main/java/demodatabase/android/myapplicationdev/com/demodatabase/DBHelper.java
@@ -2,9 +2,12 @@ package demodatabase.android.myapplicationdev.com.demodatabase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -48,6 +51,64 @@ public class DBHelper extends SQLiteOpenHelper {
         // Close the database connection
         db.close();
     }
+
+    public ArrayList<String> getTaskContent() {
+        // Create an ArrayList that holds String objects
+        ArrayList<String> tasks = new ArrayList<String>();
+        // Select all the tasks' description
+        String selectQuery = "SELECT " + COLUMN_DESCRIPTION
+                + " FROM " + TABLE_TASK;
+
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                // Add the task content to the ArrayList object
+                //  0 in getString(0) return the data in the first
+                //  column in the Cursor object. getString(1)
+                //  return second column data and so on.
+                //  Use getInt(0) if data is an int
+                tasks.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_DESCRIPTION + ", "
+                + COLUMN_DATE
+                + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String description = cursor.getString(1);
+                String date = cursor.getString(2);
+                Task obj = new Task(id, description, date);
+                tasks.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+
+
 
 
     @Override
